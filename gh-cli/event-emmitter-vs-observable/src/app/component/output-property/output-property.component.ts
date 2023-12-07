32f835f6ac8a8ef766aee88ed2d1abe3df76d78a
b1010code <b1010code@gmail.com>
2023-12-07 19:45:49 -0300

    change codes (#42)

--- gh-cli/event-emmitter-vs-observable/src/app/component/output-property/output-property.component.ts
+++ gh-cli/event-emmitter-vs-observable/src/app/component/output-property/output-property.component.ts
@@ -9,7 +9,9 @@ import { PropertyService } from 'src/app/service/property.service';
 export class OutputPropertyComponent {
   @Input() valor: number = 0;
 
-  @Output() mudouValor = new EventEmitter();
+  //@Output() mudouValor = new EventEmitter();
+
+  @Output() mudouValor = new EventEmitter<{ novoValor: number }>();
 
   @Input() novoValor: number = 0;
   @Output() novoValorChange = new EventEmitter<number>();
@@ -20,27 +22,35 @@ export class OutputPropertyComponent {
 
 
   constructor(private propertyService: PropertyService) { }
-
+/*
   incrementa() {
     this.valor++;
     this.campoValorInput.nativeElement.value = this.valor;
     this.mudouValor.emit({ novoValor: this.valor });
     this.novoValorChange.emit(this.novoValor);
     console.log("novo valor : ", this.valor);
+  } */
+
+  incrementa(): void {
+    this.valorInicial++;
+    this.campoValorInput.nativeElement.value = this.valorInicial;
+    this.mudouValor.emit({ novoValor: this.valorInicial });
+    this.propertyService.setNovoValor(this.valorInicial);
+    console.log("novo valor : ", this.valorInicial);
   }
 
-  decrementa() {
-    this.valor--;
-    this.campoValorInput.nativeElement.value = this.valor;
-    this.mudouValor.emit({ novoValor: this.valor });
-    this.novoValorChange.emit(this.novoValor);
-    console.log("novo valor : ", this.valor);
+  decrementa(): void {
+    this.valorInicial--;
+    this.campoValorInput.nativeElement.value = this.valorInicial;
+    this.mudouValor.emit({ novoValor: this.valorInicial });
+    this.propertyService.setNovoValor(this.valorInicial);
+    console.log("novo valor : ", this.valorInicial);
   }
 
-  ngOnInit() {
-    this.novoValor = this.propertyService.getNovoValor();
-    this.propertyService.novoValor$.subscribe(valor => {
-      this.novoValor = valor;
-    });
+ 
+  ngOnInit(): void {
+    this.valorInicial = this.propertyService.getNovoValor();
   }
+
+
 }
--- gh-cli/event-emmitter-vs-observable/src/app/service/property.service.ts
+++ gh-cli/event-emmitter-vs-observable/src/app/service/property.service.ts
@@ -1,4 +1,3 @@
-// property.service.ts
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
